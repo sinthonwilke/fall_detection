f5c6703ad8e56867845d893aad5ba136c87d3529
Sinthon Wilke <sinthonwilke@gmail.com>
2023-12-04 16:10:24 +0700

    so far not so good

--- main.py
+++ main.py
@@ -1,29 +1,77 @@
-import cv2
 from poseDetectModule import poseDetect
+import cv2
+import time
 
 
-def main():
-    cap = cv2.VideoCapture('videoRef/1.mp4')
-    # cap = cv2.VideoCapture(1)
+class VideoProcessor:
+    def __init__(self, video_name):
+        self.video_path = (f'videoRef/{video_name}')
+        self.cap = cv2.VideoCapture(self.video_path)
+        self.pose_detector = poseDetect()
+        self.pTime = 0
+        self.key_delay = 0.06  # Adjust this value to set the key input delay
+        self.last_key_time = time.time()
 
-    poseDetector = poseDetect()
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        if poseDetector.detect(frame):
-            poseDetector.draw_landmarks()
-
+    def process_frame(self, frame):
+        cTime = time.time()
+        fps = 1 / (cTime - self.pTime)
+        self.pTime = cTime
+        cv2.putText(frame, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.imshow('Pose Estimation', frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    def handle_key_input(self, key):
+        current_time = time.time()
+        if current_time - self.last_key_time < self.key_delay:
+            return  # Skip handling the key if the delay hasn't passed
 
-    cap.release()
-    cv2.destroyAllWindows()
+        if key == 32:  # Check for the space bar key
+            self.last_key_time = current_time
+            self.pause_video()
+        elif key == ord(','):  # Comma key
+            self.last_key_time = current_time
+            self.adjust_position(-10)
+        elif key == ord('.'):  # Period key
+            self.last_key_time = current_time
+            self.adjust_position(5)
+        elif key == 27:  # Check for the 'esc' key
+            self.last_key_time = current_time
+            self.release_resources()
+
+    def pause_video(self):
+        while True:
+            key = cv2.waitKey(1) & 0xFF
+            if key == 32:  # Break the loop on space bar key
+                break
+            elif key == 27:  # Release resources and close windows on 'esc' key
+                self.release_resources()
+                return
+
+    def adjust_position(self, frames):
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.cap.get(cv2.CAP_PROP_POS_FRAMES) + frames)
+
+    def release_resources(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+    def where_magic_happens(self, frame):
+        if self.pose_detector.detect(frame):
+            landmark = self.pose_detector.get_needed_landmarks()
+
+    def run(self):
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if not ret:
+                self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                continue
+
+            self.where_magic_happens(frame)
+
+            self.process_frame(frame)
+            key = cv2.waitKey(1) & 0xFF
+            self.handle_key_input(key)
 
 
 if __name__ == '__main__':
-    main()
+    video_name = '3.mp4'
+    video_processor = VideoProcessor(video_name)
+    video_processor.run()
--- poseDetectModule.py
+++ poseDetectModule.py
@@ -1,27 +1,32 @@
-import re
 import cv2
 import mediapipe
 
 
 class poseDetect:
     def __init__(self):
-        self.min_detection_confidence = 0.2
-        self.min_tracking_confidence = 0.2
+        self.model_complexity = 1
+        self.min_detection_confidence = 0.5
+        self.min_tracking_confidence = 0.5
 
         self.mp_pose = mediapipe.solutions.pose
         self.mp_drawing = mediapipe.solutions.drawing_utils
         self.pose = self.mp_pose.Pose(
+            model_complexity=self.model_complexity,
             min_detection_confidence=self.min_detection_confidence,
             min_tracking_confidence=self.min_tracking_confidence,
         )
 
         self.frame = None
         self.pose_landmarks = None
-
         self.color = {
             'red': (0, 0, 255),
             'green': (0, 255, 0),
             'blue': (255, 0, 0),
+            'yellow': (255, 255, 0),
+            'cyan': (0, 255, 255),
+            'magenta': (255, 0, 255),
+            'white': (255, 255, 255),
+            'orange': (255, 165, 0),
         }
 
     def detect(self, frame):
@@ -36,14 +41,97 @@ class poseDetect:
     def get_landmarks(self):
         return self.pose_landmarks
 
-    def draw_landmarks(self):
-        for id, landmark in enumerate(self.pose_landmarks.landmark):
-            h, w, c = self.frame.shape
-            cx, cy = int(landmark.x * w), int(landmark.y * h)
+    def get_needed_landmarks(self):
+        head_landmark = self.pose_landmarks.landmark[0]
+        head = {
+            'x': head_landmark.x,
+            'y': head_landmark.y,
+            # 'z': head_landmark.z,
+            # 'visibility': head_landmark.visibility,
+        }
+
+        left_shoulder = self.pose_landmarks.landmark[11]
+        right_shoulder = self.pose_landmarks.landmark[12]
+        shoulder = {
+            'x': (left_shoulder.x + right_shoulder.x) / 2,
+            'y': (left_shoulder.y + right_shoulder.y) / 2,
+            # 'z': (left_shoulder.z + right_shoulder.z) / 2,
+            # 'visibility': (left_shoulder.visibility + right_shoulder.visibility) / 2
+        }
+
+        left_hip = self.pose_landmarks.landmark[23]
+        right_hip = self.pose_landmarks.landmark[24]
+        hip = {
+            'x': (left_hip.x + right_hip.x) / 2,
+            'y': (left_hip.y + right_hip.y) / 2,
+            # 'z': (left_hip.z + right_hip.z) / 2,
+            # 'visibility': (left_hip.visibility + right_hip.visibility) / 2
+        }
+
+        left_knee = self.pose_landmarks.landmark[25]
+        right_knee = self.pose_landmarks.landmark[26]
+        knee = {
+            'x': (left_knee.x + right_knee.x) / 2,
+            'y': (left_knee.y + right_knee.y) / 2,
+            # 'z': (left_knee.z + right_knee.z) / 2,
+            # 'visibility': (left_knee.visibility + right_knee.visibility) / 2
+        }
+
+        left_foot = self.pose_landmarks.landmark[31]
+        right_foot = self.pose_landmarks.landmark[32]
+        foot = {
+            'x': (left_foot.x + right_foot.x) / 2,
+            'y': (left_foot.y + right_foot.y) / 2,
+            # 'z': (left_foot.z + right_foot.z) / 2,
+            # 'visibility': (left_foot.visibility + right_foot.visibility) / 2
+        }
+
+        h, w, _ = self.frame.shape
+
+        head_point = (int(head['x'] * w), int(head['y'] * h))
+        shoulder_point = (int(shoulder['x'] * w), int(shoulder['y'] * h))
+        hip_point = (int(hip['x'] * w), int(hip['y'] * h))
+        knee_point = (int(knee['x'] * w), int(knee['y'] * h))
+        foot_point = (int(foot['x'] * w), int(foot['y'] * h))
+
+        cv2.line(self.frame, head_point, shoulder_point, self.color['red'], 2)
+        cv2.line(self.frame, shoulder_point, hip_point, self.color['green'], 2)
+        cv2.line(self.frame, hip_point, knee_point, self.color['blue'], 2)
+        cv2.line(self.frame, knee_point, foot_point, self.color['yellow'], 2)
+        cv2.circle(self.frame, head_point, 5, self.color['cyan'], cv2.FILLED)
+        cv2.circle(self.frame, shoulder_point, 5, self.color['cyan'], cv2.FILLED)
+        cv2.circle(self.frame, hip_point, 5, self.color['cyan'], cv2.FILLED)
+        cv2.circle(self.frame, knee_point, 5, self.color['cyan'], cv2.FILLED)
+        cv2.circle(self.frame, foot_point, 5, self.color['cyan'], cv2.FILLED)
+
+        returnValue = {
+            'head': head_point,
+            'shoulder': shoulder_point,
+            'hip': hip_point,
+            'knee': knee_point,
+            'foot': foot_point,
+        }
+
+        return returnValue
+
+    def draw_landmarks(self, fullDraw=True):
+        if fullDraw:
+            self.mp_drawing.draw_landmarks(
+                self.frame, self.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
+
+    def draw_id_landmarks(self, specificLandmark=[]):
+        h, w, _ = self.frame.shape
+
+        if specificLandmark == []:
+            for id in range(33):
+                specificLandmark.append(id)
+
+        for id in specificLandmark:
+            cx, cy = int(
+                self.pose_landmarks.landmark[id].x * w), int(self.pose_landmarks.landmark[id].y * h)
             text = f'id: {id}'
             cv2.putText(self.frame, text, (cx, cy + 20),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.50, self.color['red'], 1)
-        self.mp_drawing.draw_landmarks(self.frame, self.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
 
     def draw_bbox(self, text=False, color='green', margin=0):
         bbox = self.calculate_bbox(margin)
@@ -54,12 +142,11 @@ class poseDetect:
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.color[color], 2)
 
     def calculate_bbox(self, margin=0):
-        if self.pose_landmarks is not None:
-            h, w, _ = self.frame.shape
-            all_x = [int(lm.x * w) for lm in self.pose_landmarks.landmark]
-            all_y = [int(lm.y * h) for lm in self.pose_landmarks.landmark]
-            bbox_min = (max(min(all_x) - margin, 0), max(min(all_y) - margin, 0))
-            bbox_max = (min(max(all_x) + margin, w), min(max(all_y) + margin, h))
-            return bbox_min, bbox_max
-        else:
-            return False
+        h, w, _ = self.frame.shape
+        all_x = [int(lm.x * w) for lm in self.pose_landmarks.landmark]
+        all_y = [int(lm.y * h) for lm in self.pose_landmarks.landmark]
+        bbox_min = (max(min(all_x) - margin, 0),
+                    max(min(all_y) - margin, 0))
+        bbox_max = (min(max(all_x) + margin, w),
+                    min(max(all_y) + margin, h))
+        return bbox_min, bbox_max
